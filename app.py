25a347b5c98400b0baa52c0f70a2db1d633514d6
stefanoamato93-wq <stefanoamato93@gmail.com>
2025-09-06 16:21:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Portfolio")
-st.subheader("Portfolio Total Value")
-st.metric("Total", f"${df['Value'].sum():,.2f}")
+
 
 
 # Example dummy data
@@ -15,6 +13,9 @@ data = {
 df = pd.DataFrame(data)
 df["Value"] = df["Quantity"] * df["Price"]
 
+st.title("Portfolio")
+st.subheader("Portfolio Total Value")
+st.metric("Total", f"${df['Value'].sum():,.2f}")
 st.subheader("Holdings")
 st.dataframe(df)
 
